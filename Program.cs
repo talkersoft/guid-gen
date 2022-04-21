749a30108598b97d9cf40282c98a00787f1c4c38
Todd Alker <talkersoft@gmail.com>
2022-04-21 16:29:01 -0400

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TemplateGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(Guid.NewGuid());
+        }
+    }
+}
