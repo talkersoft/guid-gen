518d777c64b4ff56415cb258064c03089dd8403d
Todd Alker <todd.alker@padi.com>
2022-04-22 12:32:52 -0400

    rename

--- aws-update-creds/deploy.sh
+++ aws-update-creds/deploy.sh
@@ -1,13 +1,12 @@
-﻿#!/bin/bash
+#!/bin/bash
 deploypath="/usr/local/bin/test-update-creds"
 cat << EOM > $deploypath
 awscredspath="${HOME}/.aws/credentials"
-
-cat << EOM > $awscredspath
-    [default]
-    aws_access_key_id=\${AWS_ACCESS_KEY_ID}
-    aws_secret_access_key=\${AWS_SECRET_ACCESS_KEY}
-    aws_session_token=\${AWS_SESSION_TOKEN}
-    \EOM
+cat << EOM > \$awscredspath
+[default]
+aws_access_key_id=\${AWS_ACCESS_KEY_ID}
+aws_secret_access_key=\${AWS_SECRET_ACCESS_KEY}
+aws_session_token=\${AWS_SESSION_TOKEN}
+$(echo EOM)
 EOM
 chmod +x $deploypath
